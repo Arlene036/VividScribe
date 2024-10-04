ce693b6d449e5a279d7a1def6a72559463e44050
Zengliang Zhu <zhuzengliangll@163.com>
2024-10-04 19:28:33 -0400

    remove valor

--- VALOR/optim/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Copyright (c) Microsoft Corporation.
-Licensed under the MIT license.
-
-"""
-from .sched import  warmup_linear, get_lr_sched
-from .adamw import AdamW
--- VALOR/preinstall.sh
+++
@@ -1,7 +0,0 @@
-pip install tensorboardx
-pip install toolz
-pip install ipdb
-pip install matplotlib
-pip install pytorch_pretrained_bert
-pip install easydict
-pip install pyyaml
