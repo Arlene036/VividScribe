a5f3e54be1c65b817f7b3ba90332b9327cec89bf
Yaqi Wang <614223070@qq.com>
2024-10-06 01:27:11 -0400

    update download_from_drive.sh for mix120

--- download_from_drive.sh
+++ download_from_drive.sh
@@ -1,17 +1,13 @@
 # download the data from the google drive
-
 pip3 install gdown
-cd data/test120
-
-# download sample_v_nv_test120_new.json
-gdown 12qI5xzSdgE0tGCaGjMmTDe1oPsI-51ge
+cd data/mix120
 
 # download and unzip raw_video.zip
-gdown 1tI5KpsBh7hj6mwYG0BsMsj-6KiIxS2qG
-unzip raw_video.zip
-rm raw_video.zip
+# gdown 1j3iJ1GXYcYihijDX56TodfuKWyci7wwa
+# unzip raw_video.zip
+# rm raw_video.zip
 
 # download and unzip extracted_data.zip
-gdown 1e8-NMTbTRhSbMTXrsNzEsoyT7vHF-nNI
+gdown 1Ds_hsA42N2NcmXMEDn0GHipzNWX41JMR
 unzip extracted_data.zip
 rm extracted_data.zip
